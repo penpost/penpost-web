68d8312ab6cac5455167089d51719cba85f33dff
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-04-08 20:11:27 -0600

    Connect Home component to App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,12 @@
+import React from 'react' 
+import Home from '../Home/Home'
+
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <Home/>
     </div>
-  );
+  )
 }
 
 export default App;
