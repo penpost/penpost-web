d336bb396448d7e7b763cb937d795ed749038c51
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-04-08 20:40:15 -0600

    Clean up styling on homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,11 +4,11 @@ import './Home.scss'
 const Home = () => {
     return (
         <section className='homepage'>
-            <h1 className='homepageTitle'>Welcome to PostPal</h1>
+            <h1 className='homepageTitle'>Welcome to PenPost</h1>
             <h2>About Us</h2>
             <p>How it works</p>
             <button className='loginButton button'>Login</button>
-            <p>If you don't have a login, sign up below</p>
+            <p className='homepageParagraph'>If you don't have a login, sign up below</p>
             <button className='registerButton button'>Register</button>
         </section>
     )
