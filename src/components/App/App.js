29cff9e21eb28484cb555a893a59efa2a2b4b95e
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-04-12 15:30:20 -0600

    Update App component to reflect recent changes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,34 @@
-import React from 'react' 
+//Imports
+import React from 'react'
+import { Route } from 'react-router-dom'
+
+//Components Imports
+import Header from '../Header/Header'
 import Home from '../Home/Home'
+import Login from '../Login/Login'
+import UserLanding from '../User-Landing-Page/User-Landing'
+
+//Styling Imports
 import './_App.scss'
 
 const App = () => {
   return (
     <main>
-      <Home/>
+      <Header />
+      <Route
+      exact
+      path="/" component={Home}
+      />
+      <Route
+      exact
+      path="/login"
+      render={() => <Login/>}
+      />
+      <Route
+      exact
+      path='/user-landing'
+      render={() => <UserLanding/>}
+      />
     </main>
   )
 }
