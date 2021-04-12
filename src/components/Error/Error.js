4dc73dcec28de52ec74390f9ff03ba3995e6a47f
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-04-11 18:46:46 -0600

    Add error handling functionality

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -2,7 +2,14 @@ import React from 'react'
 import './Error.sass'
 
 const Error = () => {
-
+return (
+    <section>
+        <article>
+            <h2>We're having technical difficulties.</h2>
+            <p><em>Please refresh or revisit at a later time.</em></p>
+        </article>
+    </section>
+)
 }
 
 export default Error
