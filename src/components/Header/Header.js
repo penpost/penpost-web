c98ec6cad5b9a64d74f46ea0b0f8ecc582488c48
gaj23 <gabriellejoyce1930@gmail.com>
2021-04-12 11:57:17 -0600

    Trouble shoot hamburger menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import './Header.scss';
+import Hamburger from './Hamburger';
+
+import './_Header.scss';
 
 const Header = () => {
-  //must add logic for logging out here.
-
   return(
     <header>
       <h1>PenPost</h1>
-      <nav role="navigation">
-        <ul className="menu">
-          <Link to='/'><li>Home</li></Link>
-          <Link to='/login'><li>Logout</li></Link>
-        </ul>
-      </nav>
+      <Hamburger />
     </header>
   )
 }
