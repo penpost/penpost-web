23a7dec6032c96ca96559f6b5d66839457ecd39c
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-04-14 15:20:30 -0600

    Add import links from Apollo client

--- src/index.js
+++ src/index.js
@@ -2,12 +2,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from } from "@apollo/client";
+import { onError } from '@apollo/client/link'
+import { cache } from "./cache";
+import Pages from "./pages";
+import injectStyles from "./styles";
 
 //Component Imports
 import App from './components/App/App';
 
 //UI Imports
 import './index.scss';
+
+// Initialize ApolloClient
+const client = new ApolloClient({
+  cache,
+  uri: "http://localhost:4000/graphql"
+});
 
 const router = (
   <BrowserRouter>
@@ -16,6 +27,8 @@ const router = (
 );
 
 ReactDOM.render(
-  router,
-  document.getElementById('root')
+  <ApolloProvider client={client}>
+    router
+  </ApolloProvider>,
+    document.getElementById('root')
 );
