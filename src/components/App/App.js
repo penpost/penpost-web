5aa71d09a2c6d7c51cc5cf3eded0153bc18c15e2
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-04-13 20:11:40 -0600

    Add error component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,28 +2,19 @@ import React from 'react'
 import Home from '../Home/Home'
 import Login from '../Login/Login'
 import UserLanding from '../User-Landing-Page/User-Landing'
+import Error from '../Error/Error'
 import './_App.scss'
 import { Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <main>
-      <Route 
-      exact
-      path="/" component={Home}
-      />
-      <Route
-      exact
-      path="/login"
-      render={() => <Login/>} 
-      />
-      <Route 
-      exact
-      path='/user-landing'
-      render={() => <UserLanding/>}
-      />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" render={() => <Login />} />
+      <Route exact path="/user-landing" render={() => <UserLanding />} />
+      <Route exact path="/error" render={() => <Error />} />
     </main>
-  )
+  );
 }
 
 export default App;
