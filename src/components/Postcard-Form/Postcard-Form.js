117eafd0bb8eacb2b3abbaf5c2360b6c04337549
Jeff Kersting <jeff.w.kersting@gmail.com>
2021-04-13 19:02:44 -0600

    Minor styling changes

--- src/components/Postcard-Form/Postcard-Form.js
+++ src/components/Postcard-Form/Postcard-Form.js
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types';
 
 const PostcardForm = () => {
 
-  const [image, setImage] = useState('')
-  const [message, setMessage] = useState('')
+  const [image, setImage] = useState(null)
+  const [message, setMessage] = useState(null)
   const location = useLocation()
 
   useEffect(() => {
@@ -28,21 +28,23 @@ const PostcardForm = () => {
     }
   }
 
-
   return (
     <section className='postcard-form'>
       <h2>Create your postcard here!</h2>
-      <form onChange={inputHandler}>
+      <form onChange={inputHandler} noValidate>
         <h3>Upload and select an image</h3>
         <div>
           <label htmlFor='image' value='image upload'/>
-          <input required type='text' value={image} id='image'/>
+          <input type='text' value={image} id='image' required/>
           <label htmlFor='clear-image' value='clear image'/>
           <button type='button' id='clear-image' onClick={() => setImage('')}>X</button>
         </div>
+        <section className='image-thumbnail'>
+          {image && <img src={image} alt='image thumbnail' />}
+        </section>
         <h3>Enter your message below</h3>
         <label htmlFor='message' value='message' />
-        <textarea type='text' maxLength='400' id='message' value={message} />
+        <textarea type='text' maxLength='400' id='message' value={message} required/>
         <label htmlFor='preview' value='preview postcard' />
         <Link to={{
           pathname: '/preview-postcard',
@@ -51,7 +53,7 @@ const PostcardForm = () => {
             message
           }
         }}>
-          <input required type='submit' value='Preview' id='preview'/>
+          <input type='submit' value='Preview' id='preview' />
         </Link>
       </form>
     </section>
