5c0d6e2683c56bbfb11ce0c1ca457a7e032dbbef
Gabrielle Joyce <68332132+gaj23@users.noreply.github.com>
2021-04-16 12:39:24 -0600

    Correct file path name

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,7 +6,7 @@ import { Route } from 'react-router-dom';
 import Header from '../Header/Header';
 import Home from '../Home/Home';
 import Login from '../Login/Login';
-import UserLanding from '../User-Landing-Page/User-Landing';
+import UserLanding from '../User-Landing/User-Landing';
 import PostcardForm from '../Postcard-Form/Postcard-Form';
 import Preview from '../Postcard-Preview/Postcard-Preview';
 import Error from '../Error/Error';
