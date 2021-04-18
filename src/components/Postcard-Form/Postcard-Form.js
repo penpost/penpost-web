80c202381fe9c8d8d24564a753488a10d628cb3b
Gabrielle Joyce <68332132+gaj23@users.noreply.github.com>
2021-04-18 17:14:39 -0600

    Manage react warnings in console

--- src/components/Postcard-Form/Postcard-Form.js
+++ src/components/Postcard-Form/Postcard-Form.js
@@ -1,5 +1,5 @@
 // Imports
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
@@ -8,16 +8,19 @@ import stamp from '../assets/postage-stamp.png';
 
 const PostcardForm = () => {
 
-  const [image, setImage] = useState(null)
-  const [message, setMessage] = useState(null)
-  const location = useLocation()
+  const [image, setImage] = useState(null);
+  const [message, setMessage] = useState(null);
+  const location = useLocation();
 
-  useEffect(() => {
-    if (location.state !== undefined) {
-      setImage(location.state.image)
-      setMessage(location.state.message)
-    }
-  }, [])
+  if (location.state !== undefined) {
+    setImage(location.state.image)
+    setMessage(location.state.message)
+  }
+
+  if (image === null || message === null) {
+    setImage('')
+    setMessage('')
+  }
 
   const inputHandler = (e) => {
     switch (e.target.id) {
@@ -32,8 +35,6 @@ const PostcardForm = () => {
     }
   }
 
-  //cannot be a gif!
-
   return (
     <section className='postcard-form'>
       <h2>Create your postcard here!</h2>
@@ -42,7 +43,7 @@ const PostcardForm = () => {
         <p>(Please use a link)</p>
         <article>
           <label htmlFor='image' value='image upload'/>
-          <input type='text' value={image} id='image' required/>
+          <input type='text' value={image} id='image' onChange={inputHandler} required/>
           <label htmlFor='clear-image' value='clear image' />
           <button type='button' id='clear-image' onClick={() => setImage('')}>X</button>
         </article>
@@ -68,6 +69,7 @@ const PostcardForm = () => {
           maxLength='400'
           id='message'
           value={message}
+          onChange={inputHandler}
           required
         />
         <label htmlFor='preview' value='preview postcard' />
