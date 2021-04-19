2522fca0606100de3c4be095e356ce64302bba4b
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-04-19 15:58:15 -0600

    Add testing for header/nav bar component

--- cypress/integration/Home_spec.js
+++ cypress/integration/Home_spec.js
@@ -23,5 +23,29 @@ describe('Homepage', () => {
   it("Should be able to click the login button", () => {
     cy.get(".loginButton").should("contain", "Login").click();
   });
+})
+
+describe('Navbar', () => {
+  beforeEach(() => {
+    cy.visit(baseURL)
+  })
+
+  it('Should have a logo of Penpost in the header', () => {
+    cy.get(".headerTitle").should('contain', 'PenPost')
+  })
+
+  it("Should have a hamburger menu", () => {
+    cy.get(".hamburger-react").should('be.visible');
+  })
+
+  it("Should be able to click on the hamburger menu and the menu will open", () => {
+    cy.get(".hamburger-react").click()
+  });
+
+  it("Should display two options in the hamburger menu if you are logged out", () => {
+    cy.get(".hamburger-react").click()
+    cy.get(".menu").should('contain', 'Home', 'Login')
+  });
+
 
 })
